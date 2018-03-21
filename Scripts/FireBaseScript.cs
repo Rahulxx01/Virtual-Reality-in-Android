17a34d1df9bd0362a64f72afbbc33741d0be860c
RAHUL YADAV <ryadav531996@gmail.com>
2018-03-21 19:11:27 +0530

    First Commit
    Initial implementation of Virtual Reality In unity.

---
+++ Scripts/FireBaseScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.SceneManagement;
+
+public class FireBaseScript : MonoBehaviour {
+	public InputField EmailAddress,Password;
+
+	public void LogInButtonPressed(){
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(EmailAddress.text,Password.text).
+		ContinueWith((obj) => {
+			SceneManager.LoadSceneAsync("MainMenu");
+		});
+			
+	}
+
+	public void LogInGooglePlus(){
+		//FirebaseAuth.DefaultInstance.S
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
