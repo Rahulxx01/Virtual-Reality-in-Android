ef17a3b637001a7a7df948b28c6d34f3ab881f0a
RAHUL YADAV <ryadav531996@gmail.com>
2018-03-26 20:26:38 +0530

    2nd Commit
    Second Commit

---
+++ Scripts/GazeInterationScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GazeInterationScript : MonoBehaviour {
+
+	// Use this for initialization
+	public float MyTime = 0f;
+	public int sceneInteger;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+
+		MyTime += Time.deltaTime;
+		if(MyTime >= 3f){
+			changeScreen(sceneInteger);
+		}
+		
+	}
+
+	public void changeScreen(int changeScene){
+		SceneManager.LoadScene (changeScene);
+	}
+
+
+}
---
+++ Scripts/Interaction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour {
+	public Material look;
+	public Material notLook;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material = notLook;
+	}
+	
+	public void TotallyWatching(){
+		GetComponent<Renderer> ().material = look;
+	}
+
+	public void notWatching(){
+		GetComponent<Renderer> ().material = notLook;
+	}
+}
